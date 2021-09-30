df06199fb96b3e7c545d3687cddac943de61f0ba
Славное Пузо <koi196621@gmail.com>
2021-09-30 20:50:02 +0300

    Первый комент

--- src/main/java/toppan/example/toppan/controllers/BlogController.java
+++ src/main/java/toppan/example/toppan/controllers/BlogController.java
@@ -36,7 +36,7 @@ public class BlogController {
 
     @PostMapping("/blog-add")
     public String blogPostAdd(@RequestParam String title, @RequestParam String anons,@RequestParam String full_text, HttpServletRequest request, Model model) {
-        HttpServletRequest.
+//        HttpServletRequest.
         Post post = new Post(title,anons,full_text);
         postRepository.save(post);
         return "redirect:/blog-main";
