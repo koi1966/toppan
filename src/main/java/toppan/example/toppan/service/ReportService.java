d22fcfa6f238b8d23ea127cc194daafe350e4aca
Oleg Klimchuk <koi196621@gmail.com>
2023-04-27 17:00:49 +0300

    Added new functionality for working with local data of the regional database.(Update info from table Sybase.gai.arest to Postgres.rsc1840.arest)  27.04.2023

--- src/main/java/toppan/example/toppan/service/ReportService.java
+++ src/main/java/toppan/example/toppan/service/ReportService.java
@@ -6,4 +6,5 @@ public interface ReportService {
     void createMonthlyReport(LocalDate from, LocalDate to, String tsc);
 
     void createWeekReportTSC(LocalDate from, LocalDate to, String tsc);
+
 }
