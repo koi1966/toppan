236cd5f645a99a8cf71597e584e26c8effa724dd
Oleg Klimchuk <koi196621@gmail.com>
2023-06-28 17:16:24 +0300

    Added new functionality for working with local data of the regional database.(Security)  28.06.2023

--- src/main/java/toppan/example/toppan/controllers/MainController.java
+++ src/main/java/toppan/example/toppan/controllers/MainController.java
@@ -5,13 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Slf4j   // Логер
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "РСЦ ГСЦ в Житомирскій області");
+        model.addAttribute("title", "РСЦ ГСЦ в Житомирскій області__ооо");
+        model.addAttribute("standardDate", new Date());
+
         return "Home";
     }
 
