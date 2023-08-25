fab9ba2ba4f2f934bb188e5695464b0496f238d5
Oleg Klimchuk <koi196621@gmail.com>
2023-08-25 17:08:12 +0300

    Added new functionality for working with local data of the regional database.(Security)  25.08.2023 (Master - NotSecyrity)

--- src/main/java/toppan/example/toppan/models/repo/UserDao.java
+++
@@ -1,8 +0,0 @@
-package toppan.example.toppan.models.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import toppan.example.toppan.models.User;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
