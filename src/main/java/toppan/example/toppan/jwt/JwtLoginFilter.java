7bf3528213d5c91d1a2dcddf17940292bbc52687
Oleg Klymchyk <koi196621@gmail.com>
2024-06-09 19:40:14 +0300

    09.06.2024 work has been completed to improve the safety of the project.

--- src/main/java/toppan/example/toppan/jwt/JwtLoginFilter.java
+++ src/main/java/toppan/example/toppan/jwt/JwtLoginFilter.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
